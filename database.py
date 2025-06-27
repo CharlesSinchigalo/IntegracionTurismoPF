d8e678959cabe2b864a2c835270a6229855f15d7
CharlesSinchigalo <147677534+CharlesSinchigalo@users.noreply.github.com>
2025-06-27 15:08:40 -0500

    Backend initial commit

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-# URL de Railway
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:mWxCGtpzWDLhKGFgbhpeYFtsTkfotabq@crossover.proxy.rlwy.net:40472/turismo_db"
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
