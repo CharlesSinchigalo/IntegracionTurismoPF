b09dab2d942ae741098e7ee42a6892f907c2542b
CharlesSinchigalo <147677534+CharlesSinchigalo@users.noreply.github.com>
2025-06-27 15:35:09 -0500

    fix: update env for Railway DB

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
+from dotenv import load_dotenv
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
-from dotenv import load_dotenv
 
 load_dotenv()
 
